34bce9b601a07e84dfcd0e865fe7494b37700e7e
Nihal Rahman <rahmannihal72@gmail.com>
2025-06-12 20:51:22 +0530

    Generative AI to fix text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState,useEffect } from "react"
 import FileUploadCard from "@/components/FileUploadCard"
 import OutputCard from "@/components/OutputCard"
-import {CleanJson} from "./utils/cleanedResponse";
+
 export default function OCRParser() {
   const [file, setFile] = useState<File | null>(null)
   const [isProcessing, setIsProcessing] = useState(false)
